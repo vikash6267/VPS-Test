8ac4dfd7989ca370a4a7919a9b1c147a3f617cd0
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-06-15 21:22:51 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 
-// Redux
-import rootReducer from "./redux/slices/reducer/index";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
 
-const store = configureStore({
-  reducer: rootReducer,
-});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+
+
         <App />
         <Toaster />
-      </BrowserRouter>
-    </Provider>
+    
+
   </React.StrictMode>
 );
 
