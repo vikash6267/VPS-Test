1b884d6f4c8387964606215b4de86836cbc02a18
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-06-15 21:12:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-w-screen min-h-screen flex flex-col font-montserrat">
+
+Chal gya 
+
+
+    </div>
+  );
+}
+
+export default App;
